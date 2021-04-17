ce14421f3ea7794b0592ea2e21f7ceffeceb3cd7
ferdboy <77190186+ferdboy@users.noreply.github.com>
2021-04-17 19:56:46 +0700

    Add files via upload

---
+++ developer.js
@@ -0,0 +1,14 @@
+const developer = (prefix) => { 
+	return `
+    ◪ DappaUhuy
+  │
+  ├─ ❏ WhatsApp
+  ├─ ❏ 085876330812
+  ├─ ❏ Youtube
+  ├─ ❏ https://www.youtube.com/channel/UC8IMYvk5Vpze_9kRUwdecZw
+  ├─ ❏ INSTAGRAM
+  └─ ❏ https://www.instagram.com/scytfdhd._
+`
+}
+
+exports.developer = developer
---
+++ infown.js
@@ -0,0 +1,14 @@
+const infown = () => { 
+	return `
+  ◪ DappaUhuy
+  │
+  ├─ ❏ WhatsApp
+  ├─ ❏ 085876330812
+  ├─ ❏ Youtube
+  ├─ ❏ https://www.youtube.com/channel/UC8IMYvk5Vpze_9kRUwdecZw
+  ├─ ❏ INSTAGRAM
+  └─ ❏ https://www.instagram.com/scytfdhd._
+`
+}
+
+exports.infown = infown
